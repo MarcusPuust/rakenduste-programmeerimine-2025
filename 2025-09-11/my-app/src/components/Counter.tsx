7718b17b1d735d0c49364820583c97f7e0d19a33
Marcus Puust <marcusp@tlu.ee>
2025-09-12 22:30:49 +0300

    feat(counter): add buttons to increase and decrease by 1, 25, 50, 100
    - created Counter.css for component-specific styles
    
    - moved inline styles into css file

--- 2025-09-11/my-app/src/components/Counter.tsx
+++ 2025-09-11/my-app/src/components/Counter.tsx
@@ -1,21 +1,35 @@
 import { useState } from "react"
 import "../App.css"
+import "./Counter.css" // uus css fail
 
 function Counter() {
-  const [count, setCount] = useState(0) // Iga komponendi enda jaoks olev state (algväärtus 0 on default)
-  // useState on React hook, mis võimaldab funktsionaalsel komponendil hoida ja hallata state'i
-  // Andmed kaovad ära kui refreshi teha, võimalik salvestada localStorage'i või backend'i
+  const [count, setCount] = useState(0)
+
   function increaseCounter(amount: number) {
-    // Vaja täpsustada, mis see on, mitte kasutada (any) ehk (amount: number)
     setCount(count => count + amount)
+  }
+
+  function decreaseCounter(amount: number) {
+    setCount(count => count - amount)
   }
 
   return (
     <>
       <h1>Vite + React + Marcus</h1>
       <div className="card">
-        <button onClick={() => increaseCounter(1)}> count is {count} </button>
-        count is {count}
+        <h2>count is {count}</h2>
+
+        <div className="counter-grid">
+          <button onClick={() => increaseCounter(100)}>+100</button>
+          <button onClick={() => increaseCounter(50)}>+50</button>
+          <button onClick={() => increaseCounter(25)}>+25</button>
+          <button onClick={() => increaseCounter(1)}>+1</button>
+
+          <button onClick={() => decreaseCounter(1)}>-1</button>
+          <button onClick={() => decreaseCounter(25)}>-25</button>
+          <button onClick={() => decreaseCounter(50)}>-50</button>
+          <button onClick={() => decreaseCounter(100)}>-100</button>
+        </div>
       </div>
     </>
   )
