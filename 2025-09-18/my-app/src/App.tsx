7fbcd57a05e497925a1995d75150cc4c422d020c
Marcus Puust <marcusp@tlu.ee>
2025-09-18 15:59:18 +0300

    feat(2025-09-18/my-app): scaffold Vite React-TS app with MUI + HashRouter

---
+++ 2025-09-18/my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Nupp</h1>
+      <Button variant="contained">Vajuta siia!</Button>
+    </div>
+  );
+}
+
+export default App;
