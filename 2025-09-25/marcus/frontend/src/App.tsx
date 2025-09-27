59111efd6ddb96e79c7ae4e00bab16c4c49d1231
Marcus Puust <marcusp@tlu.ee>
2025-09-27 22:11:30 +0300

    feat(frontend): center app layout

--- 2025-09-25/marcus/frontend/src/App.tsx
+++ 2025-09-25/marcus/frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import { Box } from "@mui/material";
 import Cats from "./components/Cats";
 
-function App() {
-  return <Cats />;
+export default function App() {
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        p: 2,
+      }}
+    >
+      <Box sx={{ width: "100%", maxWidth: 480, textAlign: "center" }}>
+        <Cats />
+      </Box>
+    </Box>
+  );
 }
-
-export default App;
